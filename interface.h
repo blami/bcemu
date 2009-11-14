2598d4521caa0a2de6d862726ffff7137c506f46
Ondrej Balaz <ondra@blami.net>
2009-11-14 08:55:16 +0100

    Removed. Unused.

--- interface.h
+++
@@ -1,9 +0,0 @@
-/******************************************************************************
- * bc_emu: portable video game emulator                                       *
- * Copyright © 2008-2010 Ondrej Balaz, <ondra@blami.net>                      *
- * http://www.blami.net/prj/bc_emu                                            *
- *                                                                            *
- * This is free software licensed under MIT license. See LICENSE.             *
- ******************************************************************************/
-
-/* interface.h: Interface for communitation between modules. */

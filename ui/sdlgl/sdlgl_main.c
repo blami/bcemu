4747dc7b16aa73494c5d4629a79737c39d05b658
Ondrej Balaz <ondra@blami.net>
2011-08-25 11:32:40 +0200

    Minor permission changes.

--- ui/sdlgl/sdlgl_main.c
+++ ui/sdlgl/sdlgl_main.c
@@ -60,7 +60,7 @@ int sdlgl_init()
 	as.freq = 22050;
 	as.format = AUDIO_S16; /* signed 16bit */
 	as.channels = 2;
-	as.samples = 4096;
+	as.samples = 640;//4096;
 	as.callback = sdlgl_audio_fill;
 	as.userdata = NULL;
 
